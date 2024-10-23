00feaa54fdc86a89d55e75b32201cbd4b4224f7f
Thomasperge <thomaskauffmant74@gmail.com>
2024-10-23 15:34:48 +0200

    fix: correct client properties

---
+++ src/DataFixtures/ClientFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\Client;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ClientFixtures extends Fixture
+{
+    public const PREFIX = "client#";
+    public const POOL_MIN = 0;
+    public const POOL_MAX = 10;
+
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = self::POOL_MIN; $i < self::POOL_MAX; $i++) {
+            $client = new Client();
+            $client->setName($this->faker->company);
+            $client->setCreatedAt($this->faker->dateTimeBetween('-2 year', 'now'));
+            $client->setUpdatedAt($this->faker->dateTimeBetween('-1 year', 'now'));
+            $client->setStatus($this->faker->randomElement(['on', 'off']));
+
+            $manager->persist($client);
+
+            $this->addReference(self::PREFIX . $i, $client);
+        }
+
+        $manager->flush();
+    }
+}
+
---
+++ src/Entity/Client.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Traits\StatisticsPropertiesTrait;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    use StatisticsPropertiesTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
+
