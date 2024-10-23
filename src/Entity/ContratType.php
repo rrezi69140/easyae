414083356c040652b5e5e441e7b914bea0bb827f
lucasrebl <rebouletlucas@gmail.com>
2024-10-23 12:20:19 +0200

    fix length contratType entity

--- src/Entity/ContratType.php
+++ src/Entity/ContratType.php
@@ -23,7 +23,7 @@ class ContratType
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $updatedAt = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 24)]
     private ?string $status = null;
 
     public function getId(): ?int
