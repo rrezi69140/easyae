e470059bc8a624cdfb22f905b7a7d54c791c779d
nolanr-dev <nolanribaud@gmail.com>
2024-10-23 11:44:21 +0200

    faker (pas le joueur de T1 tah LOL)

--- src/DataFixtures/ContactLinkTypeFixtures.php
+++ src/DataFixtures/ContactLinkTypeFixtures.php
@@ -5,20 +5,31 @@ namespace App\DataFixtures;
 use App\Entity\ContactLinkType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class ContactLinkTypeFixtures extends Fixture
 {
+    private Generator $faker;
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 20; $i++) {
+        $now = new \DateTime();
+
+        for ($i = 0; $i < 10; $i++) {
+            $dateCreated = $this->faker->dateTimeInInterval('-1 year', '+1 year');
+            $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
             $contactLinkType = new ContactLinkType();
             $contactLinkType
-                ->setName("ContactLinkType #".$i)
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
+                ->setName($this->faker->numerify('contact-link-type-###'))
+                ->setCreatedAt($dateCreated)
+                ->setUpdatedAt($dateUpdated)
                 ->setStatus('on')
             ;
-//             $contactLinkType->setCreatedAt(new \DateTime());
             $manager->persist($contactLinkType);
         }
 
