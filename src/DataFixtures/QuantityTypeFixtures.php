534fcea607723fc53c3b3f53ea67dba6971875a0
mgabriel <bech@BechBook>
2024-10-23 11:44:20 +0200

    feature/ added : QuantityType Entity & Fixtures

--- src/DataFixtures/QuantityTypeFixtures.php
+++ src/DataFixtures/QuantityTypeFixtures.php
@@ -2,21 +2,31 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Generator;
 use App\Entity\QuantityType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class QuantityTypeFixtures extends Fixture
 {
+    private Generator $faker;
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
-        $types = ["kebab", "frites", "euros", "roubles", "indiens"];
-        for ($i = 0; $i < count($types); $i++) {
+        $now = new \DateTime();
+
+        for ($i = 0; $i < 10; $i++) {
+            $dateCreated = $this->faker->dateTimeInInterval('-2 year', '+1 year');
+            $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
             $quantityType = new QuantityType();
             $quantityType
-                ->setName($types[$i])
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
+                ->setName($this->faker->numerify('quantity type-###'))
+                ->setCreatedAt($dateCreated)
+                ->setUpdatedAt($dateUpdated)
                 ->setStatus('on');
             $manager->persist($quantityType);
 
