68a87f5d9bc06589373e0e1d3871eeabdf2b416b
Rezig Rachid <rachid.rezig@ynov.com>
2024-10-23 12:17:57 +0200

    utilisation des faker

--- src/DataFixtures/InfoTypeFixtures.php
+++ src/DataFixtures/InfoTypeFixtures.php
@@ -2,24 +2,37 @@
 
 namespace App\DataFixtures;
 
-
+use Faker\Factory;
+use Faker\Generator;
 use App\Entity\InfoType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class InfoTypeFixtures extends Fixture
 {
+    
+    public const PREFIX = "account#";
+    public const POOL_MIN = 0;
+    public const POOL_MAX = 25;
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
+        $now = new \DateTime();
        
-       
-       for ($i = 0; $i < 20; $i++) {
+       for ($i = self::POOL_MIN; $i < self::POOL_MAX; $i++) {
+        $dateCreated = $this->faker->dateTimeInInterval('-1 year', '+1 year');
+        $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
         $infoType = new InfoType();
-       $infoType->setName("Personal $i");
-       $infoType->setInfo("Personal Info ");
-       $infoType->setCreatedAt(new \DateTime());
-       $infoType->setupdateAt(new \DateTime());
-       $infoType->setStatus('on')
+        $infoType->setName(" ".$this->faker->word());
+        $infoType->setInfo("Personal Info ");
+        $infoType->setCreatedAt($dateCreated);
+        $infoType->setupdateAt($dateUpdated);
+        $infoType->setStatus('on')
        ;
        $manager->persist($infoType);
        }
