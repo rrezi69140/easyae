d10c3cfeaca2361fbd22505b029a3506809a9a9b
fthomas <thomas.foltzer@ynov.com>
2024-10-23 12:02:16 +0200

    feat(Fonction) : Faker fixtures

--- src/DataFixtures/FonctionFixtures.php
+++ src/DataFixtures/FonctionFixtures.php
@@ -5,20 +5,29 @@ namespace App\DataFixtures;
 use App\Entity\Fonction;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class FonctionFixtures extends Fixture
 {
+    public const PREFIX = "fonction#";
+
     public function load(ObjectManager $manager): void
     {
-        $fonction = new Fonction();
+        $faker = Factory::create();
 
-        $fonction->setName("NameTest");
-        $fonction->setType("TypeTest");
-        $fonction->setCreatedAt(new \DateTime(""));
-        $fonction->setUpdatedAt(new \DateTime(""));
-        $fonction->setStatus("on");
- 
-        $manager->persist($fonction);
+        for ($i = 0; $i < 10; $i++) {
+            $fonction = new Fonction();
+
+            $fonction->setName($faker->jobTitle());  // Génère un nom de fonction aléatoire
+            $fonction->setType($faker->word());      // Génère un mot aléatoire pour le type
+            $fonction->setCreatedAt($faker->dateTimeBetween('-2 years', 'now'));  // Date aléatoire entre il y a 2 ans et aujourd'hui
+            $fonction->setUpdatedAt($faker->dateTimeBetween('-1 years', 'now'));  // Date aléatoire entre il y a 1 an et aujourd'hui
+            $fonction->setStatus($faker->randomElement(['on', 'off']));  // Statut aléatoire "on" ou "off"
+
+            $manager->persist($fonction);
+            $this->addReference(self::PREFIX,$i, $fonction);
+        }
+
         $manager->flush();
     }
 }
