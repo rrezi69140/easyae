7e16695a0306e2783bc8d7522f551d2b40a953d1
manonlbrz <manonlebarazer@icloud.com>
2024-10-23 11:31:03 +0200

    add faker product type

--- src/DataFixtures/ProductTypeFixtures.php
+++ src/DataFixtures/ProductTypeFixtures.php
@@ -2,21 +2,33 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Generator;
 use App\Entity\ProductType;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class ProductTypeFixtures extends Fixture
 {
+    private Generator $faker;
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
+        $now = new \DateTime();
+
         for ($i = 0; $i < 20; ++$i) {
+            $dateCreated = $this->faker->dateTimeInInterval('-1 year', '+1 year');
+            $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
+            
             $productType = new ProductType();
             $productType
-                ->setName("Type produit #$i")
+                ->setName($this->faker->numerify('product type-###'))
                 ->setPrice(price: 10.3 + $i)
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
+                ->setCreatedAt($dateCreated)
+                ->setUpdatedAt($dateUpdated)
                 ->setStatus('on')
             ;
             $manager->persist($productType);
