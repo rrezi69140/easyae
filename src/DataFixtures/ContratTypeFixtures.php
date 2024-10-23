4c68e91921f3304edcc652cbb8f66b161680fd65
lucasrebl <rebouletlucas@gmail.com>
2024-10-23 11:54:23 +0200

    fake data for contratType using faker

--- src/DataFixtures/ContratTypeFixtures.php
+++ src/DataFixtures/ContratTypeFixtures.php
@@ -5,20 +5,31 @@ namespace App\DataFixtures;
 use App\Entity\ContratType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Datetime;
+use Faker\Factory;
+use Faker\Generator;
 
 class ContratTypeFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $now = new \DateTime();
 
         for ($i = 0; $i < 20; $i++) {
+            $dateCreated = $this->faker->dateTimeInInterval('-1 year', '+1 year');
+            $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
             $contratType = new ContratType();
 
             $contratType
-                ->setName("contrat type" . $i)
-                ->setUpdatedAt(new DateTime())
-                ->setCreatedAt(new DateTime())
+                ->setName($this->faker->numerify('contratType-###'))
+                ->setUpdatedAt($dateCreated)
+                ->setCreatedAt($dateUpdated)
                 ->setStatus("on")
             ;
 
