767c491f6868e916e6e940a700c9b8c53bf13a14
fthomas <thomas.foltzer@ynov.com>
2024-10-23 12:28:14 +0200

    fix

--- src/DataFixtures/FonctionFixtures.php
+++ src/DataFixtures/FonctionFixtures.php
@@ -6,26 +6,34 @@ use App\Entity\Fonction;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Faker\Generator;
 
 class FonctionFixtures extends Fixture
 {
     public const PREFIX = "fonction#";
+    public const POOL_MIN = 0;
+    public const POOL_MAX = 10;
+
+    private Generator $faker;
+    private function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
 
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = self::POOL_MIN; $i < self::POOL_MAX; $i++) {
             $fonction = new Fonction();
 
-            $fonction->setName($faker->jobTitle());  
-            $fonction->setType($faker->word());     
-            $fonction->setCreatedAt($faker->dateTimeBetween('-2 years', 'now'));  
-            $fonction->setUpdatedAt($faker->dateTimeBetween('-1 years', 'now')); 
-            $fonction->setStatus($faker->randomElement(['on', 'off']));
+            $fonction->setName($this->faker->jobTitle());
+            $fonction->setType($this->faker->word());
+            $fonction->setCreatedAt($this->faker->dateTimeBetween('-2 years', 'now'));
+            $fonction->setUpdatedAt($this->faker->dateTimeBetween('-1 years', 'now'));
+            $fonction->setStatus($this->faker->randomElement(['on', 'off']));
 
             $manager->persist($fonction);
-            $this->addReference(self::PREFIX,$i, $fonction);
+            $this->addReference(self::PREFIX, $i, $fonction);
         }
 
         $manager->flush();
