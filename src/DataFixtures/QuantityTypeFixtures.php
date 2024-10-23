f00f7be891c52d106a46460176e429ee5806c073
mgabriel <bech@BechBook>
2024-10-23 11:03:37 +0200

    feature/ added : QuantityType Entity & Fixtures

--- src/DataFixtures/QuantityTypeFixtures.php
+++ src/DataFixtures/QuantityTypeFixtures.php
@@ -10,15 +10,17 @@ class QuantityTypeFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $quantityType = new QuantityType();
-        $quantityType
-            ->setName("Quantité 1")
-            ->setCreatedAt(new \DateTime())
-            ->setUpdatedAt(new \DateTime())
-            ->setStatus('on')
-        ;
-        $manager->persist($quantityType);
+        $types = ["kebab", "frites", "euros", "roubles", "indiens"];
+        for ($i = 0; $i < count($types); $i++) {
+            $quantityType = new QuantityType();
+            $quantityType
+                ->setName($types[$i])
+                ->setCreatedAt(new \DateTime())
+                ->setUpdatedAt(new \DateTime())
+                ->setStatus('on');
+            $manager->persist($quantityType);
 
-        $manager->flush();
+            $manager->flush();
+        }
     }
 }
