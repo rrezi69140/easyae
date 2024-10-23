6f6ca13c5f3a3f3f16d417af97047d40b3ed7823
Alexis <velris.dev@gmail.com>
2024-10-23 11:42:40 +0200

    Entity Product Fixtures with Faker Added

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -5,20 +5,33 @@ namespace App\DataFixtures;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class ProductFixtures extends Fixture
 {
+
+    private Generator $faker;
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
+        $now = new \DateTime();
+
         for ($i = 0; $i < 20; ++$i) {
+            $dateCreated = $this->faker->dateTimeInInterval('-1 year', '+1 year');
+            $dateUpdated = $this->faker->dateTimeBetween($dateCreated, $now);
             $product = new Product();
+            $statuses = ['OK', 'Failed', 'Pending'];
             $product
-                ->setQuantity(50 + $i)
-                ->setCreatedAt(new \DateTime())
-                ->setUpdatedAt(new \DateTime())
-                ->setStatus("Disponible $i")
-                ->setPrice(10.2 + $i)
-                ->setPriceUnit(20 + $i);
+                ->setQuantity($this->faker->randomDigit())
+                ->setCreatedAt($dateCreated)
+                ->setUpdatedAt($dateUpdated)
+                ->setStatus($statuses[rand(0, count($statuses)-1)])
+                ->setPrice($this->faker->randomFloat(2, 10, 100))
+                ->setPriceUnit($this->faker->randomFloat(2, 10, 100));
             $manager->persist($product);
         }
 
