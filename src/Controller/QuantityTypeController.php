f9b63285a993b262703edb166b603b33731455af
mgabriel <bech@BechBook>
2024-10-25 13:00:49 +0200

    #77-ft-QuantityType-post

--- src/Controller/QuantityTypeController.php
+++ src/Controller/QuantityTypeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
 use App\Entity\QuantityType;
+use App\Repository\ClientRepository;
 use App\Repository\QuantityTypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -28,5 +32,16 @@ class QuantityTypeController extends AbstractController
         $quantityTypeJson = $serializer->serialize($quantityType, 'json', ['groups' => "quantityType"]);
 
         return new JsonResponse($quantityTypeJson, JsonResponse::HTTP_OK, [], true);
+    }
+
+    #[Route(name: 'api_quantity_type_new', methods: ["POST"])]
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $quantityType = $serializer->deserialize($request->getContent(), QuantityType::class, 'json', []);
+        $quantityType->setStatus("on");
+        $entityManager->persist($quantityType);
+        $entityManager->flush();
+        $accountJson = $serializer->serialize($quantityType, 'json', ['groups' => "quantityType"]);
+        return new JsonResponse($accountJson, JsonResponse::HTTP_OK, [], true);
     }
 }
