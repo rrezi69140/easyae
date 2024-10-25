1350c6bed272ec62ac6bb2ee3630461afd62e28c
slayzerz <ibrahimallari84@gmail.com>
2024-10-25 14:18:14 +0200

    Route Post

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\ContratTypeRepository;
+use App\Repository\ClientRepository;
 use App\Entity\Contrat;
 use App\Repository\ContratRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/api/contrat')]
 
@@ -30,6 +35,27 @@ class ContratController extends AbstractController
 
         $contratJson = $serializer->serialize($contrat, 'json', ['groups' => "contrat"]);
 
+        return new JsonResponse($contratJson, JsonResponse::HTTP_OK, [], true);
+    }
+
+    #[Route(name: 'api_contrat_new', methods: ["POST"])]
+    public function create(Request $request, clientRepository $clientRepository, ContratTypeRepository $typeRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        // dd($data);
+        $contrat = $serializer->deserialize($request->getContent(), Contrat::class, 'json', []);
+        $client = $clientRepository->find($data["client"]);
+        $type = $typeRepository->find($data["contratType"]);
+        $contrat->setClient($client)
+            ->setType($type)
+            ->setDone(0)
+            ->setStartAt(new DateTime("2022-11-24T13:34:33+00:00"))
+            ->setEndAt(new DateTime("2026-04-19T09:13:23+00:00"))
+        ;
+        $entityManager->persist($contrat);
+        $entityManager->flush();
+
+        $contratJson = $serializer->serialize($contrat, 'json', ['groups' => "contrat"]);
         return new JsonResponse($contratJson, JsonResponse::HTTP_OK, [], true);
     }
 }
--- src/Entity/Contrat.php
+++ src/Entity/Contrat.php
@@ -33,9 +33,11 @@ class Contrat
     private ?bool $isDone = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['contrat'])]
     private ?\DateTimeInterface $startAt = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Groups(['contrat'])]
     private ?\DateTimeInterface $endAt = null;
 
     #[ORM\OneToOne(mappedBy: 'contrat', targetEntity: Facturation::class)]
