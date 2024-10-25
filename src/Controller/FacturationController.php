bcc575953d93c99c36f4c025f7d9503ccde6d3ee
CactusNormal7 <jules.besson7@gmail.com>
2024-10-25 16:28:21 +0200

    feat: :sparkles: update && delete

--- src/Controller/FacturationController.php
+++ src/Controller/FacturationController.php
@@ -3,10 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Facturation;
+use App\Repository\ContratRepository;
 use App\Repository\FacturationRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/facturation')]
@@ -22,12 +28,45 @@ class FacturationController extends AbstractController
         return new JsonResponse($facturationJson, JsonResponse::HTTP_OK, [], true);
     }
 
-
     #[Route(path: '/{id}', name: 'api_facturation_show', methods: ["GET"])]
     public function get(Facturation $facturation, SerializerInterface $serializer): JsonResponse
     {
         $facturationJson = $serializer->serialize($facturation, 'json', ['groups' => "facturation"]);
 
         return new JsonResponse($facturationJson, JsonResponse::HTTP_OK, [], true);
+    }
+
+    #[Route(path: '/{id}', name: 'api_facturation_edit', methods: ["PATCH"])]
+    public function update(Facturation $facturation, Request $request, UrlGeneratorInterface $urlGenerator, ContratRepository $contratRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        if (isset($data{"contrat"})) {
+            $contrat = $contratRepository->find($data["contrat"]);
+        }
+
+        $updateFacturation = $serializer->deserialize(data: $request->getContent(), type: Facturation::class, format:"json", context: [AbstractNormalizer::OBJECT_TO_POPULATE => $facturation]);
+        $updateFacturation->setcontrat($contrat ?? $updateFacturation->getcontrat())->setStatus("on");
+
+        $entityManager->persist(object: $updateFacturation);
+        $entityManager->flush();
+        $location = $urlGenerator->generate("api_facturation_show", ['id' => $updateFacturation->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        $facturationJson = $serializer->serialize(data: $updateFacturation, format: "json", context: ["groups" => "facturation"]);
+        return new JsonResponse($facturationJson, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
+    }
+
+    #[Route(path: '/{id}', name: 'api_facturation_delete', methods: ["DELETE"])]
+    public function delete(Facturation $facturation, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+
+        if (isset($data{'force'}) && $data['force'] === true) {
+            $entityManager->remove(object: $facturation);
+            $entityManager->flush();
+        }
+        $facturation->setStatus("off");
+
+        $entityManager->persist(object: $facturation);
+        $entityManager->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, []);
     }
 }
