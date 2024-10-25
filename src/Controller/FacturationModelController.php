859cc4f0d5bdec815639d41b8f26dd4206b43ce4
kruzeyy <129726934+kruzeyy@users.noreply.github.com>
2024-10-25 16:50:28 +0200

    #102 - feat : facturationModel: Update & Delete

--- src/Controller/FacturationModelController.php
+++ src/Controller/FacturationModelController.php
@@ -45,4 +45,49 @@ class FacturationModelController extends AbstractController
         $facturationModelJson = $serializer->serialize($facturationModel, 'json', ['groups' => "facturationModel"]);
         return new JsonResponse($facturationModelJson, JsonResponse::HTTP_OK, [], true);
     }
+
+    #[Route(path: "/{id}", name: 'api_facturation_model_edit', methods: ["PATCH"])]
+    public function update(facturationModel $facturationModel, UrlGeneratorInterface $urlGenerator, Request $request, ClientRepository $clientRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        if (isset($data['client'])) {
+
+            $client = $clientRepository->find($data["client"]);
+        }
+
+
+        $updatedFacturationModel = $serializer->deserialize($request->getContent(), Account::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $facturationModel]);
+        $updatedFacturationModel
+            ->setClient($client ?? $updatedFacturationModel->getClient())
+            ->setStatus("on")
+        ;
+
+        $entityManager->persist($updatedFacturationModel);
+        $entityManager->flush();
+        $facturationModelJson = $serializer->serialize($updatedFacturationModel, 'json', ['groups' => "facturationModel"]);
+        $location = $urlGenerator->generate("api_facturation_model_show", ['id' => $updatedFacturationModel->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
+    }
+
+    #[Route(path: "/{id}", name: 'api_facturation_model_delete', methods: ["DELETE"])]
+    public function delete(FacturationModel $facturationModel, UrlGeneratorInterface $urlGenerator, Request $request, ClientRepository $clientRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        if (isset($data['force']) && $data['force'] === true) {
+            $entityManager->remove($facturationModel);
+
+
+        } else {
+            $facturationModel
+                ->setStatus("off")
+            ;
+
+            $entityManager->persist($facturationModel);
+        }
+
+
+
+        $entityManager->flush();
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+    }
 }
