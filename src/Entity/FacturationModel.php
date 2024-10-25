249276fb62656975b8835320fea3d9c953f5cf53
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2024-10-25 10:47:44 +0200

    fix: Client Entty "fixed"

--- src/Entity/FacturationModel.php
+++ src/Entity/FacturationModel.php
@@ -6,8 +6,6 @@ use App\Entity\Traits\StatisticsPropertiesTrait;
 use App\Repository\FacturationModelRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use App\Repository\FacturationModelRepository;
-use App\Entity\Traits\StatisticsPropertiesTrait;
 
 #[ORM\Entity(repositoryClass: FacturationModelRepository::class)]
 #[ORM\HasLifecycleCallbacks]
