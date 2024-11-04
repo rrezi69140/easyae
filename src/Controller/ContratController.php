cc7f8c62946aebe487af9ba7ad6776b4bc74437a
quentinmel <qmeleo@gmail.com>
2024-11-04 08:42:55 +0100

    #103 - ft: Contrat: Update & Delete

--- src/Controller/ContratController.php
+++ src/Controller/ContratController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[Route('/api/contrat')]
 
@@ -54,11 +56,62 @@ class ContratController extends AbstractController
             ->setDone($done)
             ->setStartAt($start)
             ->setEndAt($end)
+            ->setStatus("on")
         ;
         $entityManager->persist($contrat);
         $entityManager->flush();
 
         $contratJson = $serializer->serialize($contrat, 'json', ['groups' => "contrat"]);
-        return new JsonResponse($contratJson, JsonResponse::HTTP_OK, [], true);
+        return new JsonResponse($contratJson, JsonResponse::HTTP_CREATED, [], true);
+    }
+
+    #[Route(path: "/{id}", name: 'api_contrat_edit', methods: ["PATCH"])]
+    public function update(Contrat $contrat, UrlGeneratorInterface $urlGenerator, Request $request, clientRepository $clientRepository, ContratTypeRepository $typeRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        if (isset($data['client'])) {
+            $client = $clientRepository->find($data["client"]);
+        }
+        if (isset($data['contratType'])) {
+            $type = $typeRepository->find($data["contratType"]);
+        }
+        if (isset($data['is_done'])) {
+            $done = $data["is_done"];
+        }
+        if (isset($data['startAt'])) {
+            $start = new DateTime($data["startAt"]);
+        }
+        if (isset($data['endAt'])) {
+            $end = new DateTime($data["endAt"]);
+        }
+        $updatedContrat = $serializer->deserialize($request->getContent(), Contrat::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $contrat]);
+        $updatedContrat->setClient($client ?? $updatedContrat->getClient())
+            ->setType($type ?? $updatedContrat->getType())
+            ->setDone($done ?? $updatedContrat->isDone())
+            ->setStartAt($start ?? $updatedContrat->getStartAt())
+            ->setEndAt($end ?? $updatedContrat->getEndAt())
+            ->setStatus("on")
+        ;
+        $entityManager->persist($contrat);
+        $entityManager->flush();
+
+        $location = $urlGenerator->generate("api_contrat_show", ['id' => $updatedContrat->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
+    }
+
+    #[Route(path: "/{id}", name: 'api_contrat_delete', methods: ["DELETE"])]
+    public function delete(Contrat $contrat, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = $request->toArray();
+        if (isset($data['force']) && $data['force'] === true) {
+            $entityManager->remove($contrat);
+        } else {
+            $contrat->setStatus("off");
+            $entityManager->persist($contrat);
+        }
+
+        $entityManager->flush();
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
