f5a22dd13139957159bcf1fb7d0e1d689c20443e
mgabriel <bech@BechBook>
2024-11-05 15:36:20 +0100

    #110 - ft:QuantityType: Cache

--- src/Controller/QuantityTypeController.php
+++ src/Controller/QuantityTypeController.php
@@ -14,18 +14,25 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[Route('/api/quantity-type')]
 
 class QuantityTypeController extends AbstractController
 {
     #[Route(name: 'api_quantity_type_index', methods: ["GET"])]
-    public function getAll(QuantityTypeRepository $quantityTypeRepository, SerializerInterface $serializer): JsonResponse
+    public function getAll(QuantityTypeRepository $quantityTypeRepository, SerializerInterface $serializer,TagAwareCacheInterface $cache): JsonResponse
     {
-        $quantityTypeList = $quantityTypeRepository->findAll();
-
-        $quantityTypeJson = $serializer->serialize($quantityTypeList, 'json', ['groups' => "quantityType"]);
-
+        $idCache = "getAllQuantityType";
+        $quantityTypeJson = $cache->get($idCache, function (ItemInterface $item) use ($quantityTypeRepository, $serializer) {
+            $item->tag("quantityType");
+            //$item->tag("product");
+            $quantityTypeList = $quantityTypeRepository->findAll();
+            $quantityTypeJson = $serializer->serialize($quantityTypeList, 'json', ['groups' => "quantityType"]);
+            return $quantityTypeJson;
+        });
         return new JsonResponse($quantityTypeJson, JsonResponse::HTTP_OK, [], true);
     }
     #[Route(path: '/{id}', name: 'api_quantity_type_show', methods: ["GET"])]
@@ -37,30 +44,39 @@ class QuantityTypeController extends AbstractController
     }
 
     #[Route(name: 'api_quantity_type_new', methods: ["POST"])]
-    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function create(ValidatorInterface $validator, TagAwareCacheInterface $cache, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $quantityType = $serializer->deserialize($request->getContent(), QuantityType::class, 'json', []);
         $quantityType->setStatus("on");
+        $errors = $validator->validate($quantityType);
+        if (count($errors) > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
         $entityManager->persist($quantityType);
         $entityManager->flush();
+        $cache->invalidateTags(["quantityType"]);
         $accountJson = $serializer->serialize($quantityType, 'json', ['groups' => "quantityType"]);
         return new JsonResponse($accountJson, JsonResponse::HTTP_OK, [], true);
     }
 
     #[Route(path: "/{id}", name: 'api_quantity_type_edit', methods: ["PATCH"])]
-    public function update(QuantityType $quantityType, UrlGeneratorInterface $urlGenerator, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function update(TagAwareCacheInterface $cache, QuantityType $quantityType, UrlGeneratorInterface $urlGenerator, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
         $updatedQuantityType = $serializer->deserialize($request->getContent(), QuantityType::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $quantityType]);
         $updatedQuantityType->setStatus("on");
 
         $entityManager->persist($updatedQuantityType);
         $entityManager->flush();
+        $cache->invalidateTags(["quantityType"
+            //, "product"
+        ]);
+
         $location = $urlGenerator->generate("api_quantity_type_show", ['id' => $updatedQuantityType->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
     }
 
     #[Route(path: "/{id}", name: 'api_quantity_type_delete', methods: ["DELETE"])]
-    public function delete(QuantityType $quantityType, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(TagAwareCacheInterface $cache, QuantityType $quantityType, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = $request->toArray();
         if (isset($data['force']) && $data['force'] === true) {
@@ -73,6 +89,7 @@ class QuantityTypeController extends AbstractController
             ;
             $entityManager->persist($quantityType);
         }
+        $cache->invalidateTags(["quantityType"]);
         $entityManager->flush();
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
