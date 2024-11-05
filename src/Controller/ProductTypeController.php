2e6608bc888be549535cd94a72dc65c34bd0a0e5
manonlbrz <manonlebarazer@icloud.com>
2024-11-05 15:35:50 +0100

    add cache

--- src/Controller/ProductTypeController.php
+++ src/Controller/ProductTypeController.php
@@ -4,10 +4,18 @@ namespace App\Controller;
 
 use App\Entity\ProductType;
 use App\Repository\ProductTypeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/api/productType')]
@@ -15,14 +23,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductTypeController extends AbstractController
 {
     #[Route(name: 'app_product_type_index', methods: ["GET"])]
-    public function getAll(ProductTypeRepository $productTypeRepository, SerializerInterface $serializer): JsonResponse
+    #[IsGranted("ROLE_USER", message: "Hanhanhaaaaan vous n'avez pas dit le mot magiiiiqueeuuuuuh")]
+    public function getAll(ProductTypeRepository $productTypeRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
-        $productTypeList = $productTypeRepository->findAll();
+        $idCache = "getAllProductType";
+        $productTypeJson = $cache->get($idCache, function (ItemInterface $item) use ($productTypeRepository, $serializer) {
+            $item->tag("productType");
+            $productTypeList = $productTypeRepository->findAll();
+            $productTypeJson = $serializer->serialize($productTypeList, 'json', ['groups' => "productType"]);
 
-        $productTypeJson = $serializer->serialize($productTypeList, 'json', ['groups' => 'productType']);
+            return $productTypeJson;
+        });
 
         return new JsonResponse($productTypeJson, Response::HTTP_OK, [], true);
     }
+
     #[Route(path: '/{id}', name: 'api_product_type', methods: ["GET"] )]
     public function get(ProductType $productType, SerializerInterface $serializer):JsonResponse
     {
@@ -32,14 +47,22 @@ class ProductTypeController extends AbstractController
     }
 
     #[Route(name: 'api_product_type_new', methods: ["POST"])]
-    public function create(Request $request, ProductTypeRepository $productTypeRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function create(ValidatorInterface $validator, TagAwareCacheInterface $cache, Request $request, ProductTypeRepository $productTypeRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
     {
+        $data = $request->toArray();
         $productType = $serializer->deserialize($request->getContent(), ProductType::class, 'json', []);
         $productType->setClient($productType)
             ->setStatus("on")
         ;
+
+        $errors = $validator->validate($productType);
+        if (count($errors) > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+
+        }
         $entityManager->persist($productType);
         $entityManager->flush();
+        $cache->invalidateTags(["productType"]);
         $productTypeJson = $serializer->serialize($productType, 'json', ['groups' => "productType"]);
         return new JsonResponse($productTypeJson, JsonResponse::HTTP_OK, [], true);
     }
@@ -62,11 +85,13 @@ class ProductTypeController extends AbstractController
 
         $entityManager->persist($updatedProductType);
         $entityManager->flush();
+
         $location = $urlGenerator->generate("api_product_type", ['id' => $updatedProductType->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
     }
+
     #[Route(path: "/{id}", name: 'api_product_type_delete', methods: ["DELETE"])]
-    public function delete(ProductType $productType, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function delete(TagAwareCacheInterface $cache, ProductType $productType, Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = $request->toArray();
         if (isset($data['force']) && $data['force'] === true) {
@@ -84,6 +109,7 @@ class ProductTypeController extends AbstractController
 
 
         $entityManager->flush();
+        $cache->invalidateTags(["productType"]);
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 
