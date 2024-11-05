c3c665e72f2712a5fd42f564e83f158465307e4d
Thomasperge <thomaskauffmant74@gmail.com>
2024-11-05 15:31:18 +0100

    feat: addding cache

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -12,16 +12,23 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[Route('/api/client')]
 class ClientController extends AbstractController
 {
     #[Route(name: 'api_client_index', methods: ["GET"])]
-    public function getAll(ClientRepository $clientRepository, SerializerInterface $serializer): JsonResponse
+    public function getAll(ClientRepository $clientRepository, SerializerInterface $serializer, TagAwareCacheInterface $cache): JsonResponse
     {
-        $clientList = $clientRepository->findAll();
+        $idCache = "getAllClients";
 
-        $clientJson = $serializer->serialize($clientList, 'json', ['groups' => ["client", "clientType"]]);
+        $clientJson = $cache->get($idCache, function (ItemInterface $item) use ($clientRepository, $serializer) {
+            $item->tag("client");
+
+            $clientList = $clientRepository->findAll();
+            return $serializer->serialize($clientList, 'json', ['groups' => ["client", "clientType"]]);
+        });
 
         return new JsonResponse($clientJson, JsonResponse::HTTP_OK, [], true);
     }
@@ -34,16 +41,13 @@ class ClientController extends AbstractController
         }
 
         $clientJson = $serializer->serialize($client, 'json', ['groups' => ["client"]]);
-
         return new JsonResponse($clientJson, JsonResponse::HTTP_OK, [], true);
     }
 
-
     #[Route(name: 'api_client_new', methods: ["POST"])]
-    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    public function create(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $client = $serializer->deserialize($request->getContent(), Client::class, 'json');
-
         if (!$client) {
             return new JsonResponse(['error' => 'Invalid data'], JsonResponse::HTTP_BAD_REQUEST);
         }
@@ -60,37 +64,43 @@ class ClientController extends AbstractController
         $entityManager->persist($client);
         $entityManager->flush();
 
+        $cache->invalidateTags(["client"]);
+
         $clientJson = $serializer->serialize($client, 'json', ['groups' => "client"]);
         return new JsonResponse($clientJson, JsonResponse::HTTP_CREATED, [], true);
     }
 
-    #[Route(path: "/{id}", name: 'api_quantity_type_edit', methods: ["PATCH"])]
-    public function update(Client $client, UrlGeneratorInterface $urlGenerator, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager): JsonResponse
+    #[Route(path: "/{id}", name: 'api_client_edit', methods: ["PATCH"])]
+    public function update(Client $client, UrlGeneratorInterface $urlGenerator, Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
-        $updatedclient = $serializer->deserialize($request->getContent(), Client::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $client]);
-        $updatedclient->setStatus("on");
+        $updatedClient = $serializer->deserialize($request->getContent(), Client::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $client]);
+        $updatedClient->setStatus("on");
 
-        $entityManager->persist($updatedclient);
+        $entityManager->persist($updatedClient);
         $entityManager->flush();
-        $location = $urlGenerator->generate("api_quantity_type_show", ['id' => $updatedclient->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $cache->invalidateTags(["client"]);
+
+        $location = $urlGenerator->generate("api_client_show", ['id' => $updatedClient->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT, ["Location" => $location]);
     }
 
-    #[Route(path: "/{id}", name: 'api_quantity_type_delete', methods: ["DELETE"])]
-    public function delete(client $client, Request $request, EntityManagerInterface $entityManager): JsonResponse
+    #[Route(path: "/{id}", name: 'api_client_delete', methods: ["DELETE"])]
+    public function delete(Client $client, Request $request, EntityManagerInterface $entityManager, TagAwareCacheInterface $cache): JsonResponse
     {
         $data = $request->toArray();
+
         if (isset($data['force']) && $data['force'] === true) {
             $entityManager->remove($client);
-
-
         } else {
-            $client
-                ->setStatus("off")
-            ;
+            $client->setStatus("off");
             $entityManager->persist($client);
         }
+
         $entityManager->flush();
+
+        $cache->invalidateTags(["client"]);
+
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
